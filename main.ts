682e6f109348fca6e13809208c655281cced36f9
William-lemon <william.lemon@stu.ocsb.ca>
2025-11-14 00:02:18 +0000

    This program gets the sum of 3 numbers

---
+++ main.ts
@@ -0,0 +1,25 @@
+/**
+ * @author William Lemon
+ * @version 1.0.0
+ * @date 2025-11-13
+ * @fileoverview This program sums 3 constants.
+ */
+
+// intialize numbers
+const NUMBER1: number = 10;
+const NUMBER2: number = -20;
+const NUMBER3: number = 85;
+
+//INPUT - none
+
+// PROCESS
+// calculate the sum
+let answer: number = NUMBER1 + NUMBER2 + NUMBER3;
+
+// OUTPUT
+// display the result
+console.log (
+  "The sum of " + NUMBER1 + " & " + NUMBER2 + " & " + NUMBER3 + " is: " + answer
+);
+
+console.log("/nDone.");
